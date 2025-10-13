d9622ef59ab0a8ff44a73bda593aea0a981c3893
Wren Wightman <wew12@duke.edu>
2025-10-12 22:12:50 -0400

    Revert "Revert "Added warning clauses and try except blocks to allow compilation of stuff without having a CUDA device""
    This reverts commit 8a7e2bce12a60d222fb632f0177c5b20978299d6.

--- pycbf/__init__.py
+++ pycbf/__init__.py
@@ -1,5 +1,8 @@
 from pycbf.__bf_base_classes__ import Tabbed, Parallelized, __BMFRM_PARAMS__
 from pycbf.CPUBeamfromer import CPUBeamformer
 from pycbf.CPUCoherenceBeamformer import CPUCoherenceBeamformer
-
-from pycbf.GPUBeamformer import GPUBeamformer
+try:
+    from pycbf.GPUBeamformer import GPUBeamformer
+except:
+    from warnings import warn
+    warn("GPU/CUPY not detected in this environment. Module calls to GPU classes will results in module note ofund exceptions")
