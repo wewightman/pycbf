9b5368d88a0e2f14b362ed5e4b5ef5d4a826ae06
Wren Wightman <wren@firebolt>
2025-10-12 21:47:03 -0400

    Fix misspelling in coherence beamformer file name

--- pycbf/__init__.py
+++ pycbf/__init__.py
@@ -1,5 +1,5 @@
 from pycbf.__bf_base_classes__ import Tabbed, Parallelized, __BMFRM_PARAMS__
 from pycbf.CPUBeamfromer import CPUBeamformer
-from pycbf.CPUCoherenceBeamfromer import CPUCoherenceBeamformer
+from pycbf.CPUCoherenceBeamformer import CPUCoherenceBeamformer
 
 from pycbf.GPUBeamformer import GPUBeamformer
