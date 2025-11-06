Fixed diverging wave synthetic SWEI delay tab calculation

pycbf/helpers/__full_aperture_2D__.py
@@ -159,7 +159,6 @@ def make_tabs_and_apods_2D(steers, r0, ctx, cm, xele, xout, zout, fnum):
     taurx, apodrx = calc_rx_tabs_and_apods(cm, xele, xout, zout, fnum)
 
     return {"tautx":tautx, "taurx":taurx, "apodtx":apodtx, "apodrx":apodrx}
-
 def calc_rx_synthetic_points(xele, fnum):
     """Calculates the receive synthetic point parameters for a linear array based on lateral element position and f-number
 
@@ -225,8 +224,7 @@ def calc_tx_synthetic_points_pw_2D(steers, ctx, cm, xele, rpw:float=-10):
 
     return ovectx, nvectx, t0tx, doftx, alatx
 
-
-def calc_tx_synthetic_points_2D(steers, ctx, cm, xele, rpw):
+def calc_tx_synthetic_points_divergent_2D(steers, ctx, cm, xele, rpw):
     """Make delay tabs and apodizations for a 2D linear array
     
     # Parameters
@@ -234,7 +232,7 @@ def calc_tx_synthetic_points_2D(steers, ctx, cm, xele, rpw):
     - `ctx`: assumed speed of spound on transmission in m/s
     - `cm`: speed of sound in the medium
     - `xele`: lateral position of the elements in meters
-    - `rpw`: radius to approximate PW in meters
+    - `r0`: radius fro origin of probe
 
     # Returns
     - `ovectx`: the origin of the syntehtic point source
@@ -248,25 +246,28 @@ def calc_tx_synthetic_points_2D(steers, ctx, cm, xele, rpw):
 
     # Change the angle of the plane waves to do global SOS correction 
     if cm != ctx: raise NotImplementedError("Speed of sound correction has not yet been implemented for synthetic point datasets")
-
-    # point where the wave first crosses the aperture (set to be t=0)
-    origtx = np.array([[xele[-1] if steer <= 0 else xele[0] for steer in steers], np.zeros(steers.shape)]).T
-
-    # calculate normal vector of point source
-    nvectx = np.array([np.sin(steers), np.cos(steers)]).T
+    if rpw >= 0: raise Exception("Padius must ")
 
     # calcualte point origins for the sources
-    ovectx = rpw * nvectx
+    ovectx = rpw * np.array([np.sin(steers), np.cos(steers)]).T
+
+    # calcualte minimum distance from synthetic point to aperture
+    dxmin = np.min(np.hypot(xele[None,:] - ovectx[:,0,None], ovectx[:,1,None]), axis=1)
 
     # calculate the time at which the wave crosses the synthetic point source
-    t0tx = np.sum(nvectx * (ovectx - origtx), axis=-1)/cm
+    t0tx = -dxmin/cm
+
+    alneg = np.atan2(xele[0] - ovectx[:,0], -ovectx[:,1])
+    alpos = np.atan2(xele[-1] - ovectx[:,0], -ovectx[:,1])
+
+    alatx = (alpos - alneg)/2
+    alsteers = (alpos + alneg)/2
+
+    # calculate normal vector of point source
+    nvectx = np.array([np.sin(alsteers), np.cos(alsteers)]).T
 
     # depth of field around the point source to consider
     doftx = np.zeros(len(t0tx), dtype=np.float32)
-
-    # calculate acceptance angle for plane wave source approximated as a point
-    dxo = origtx - ovectx
-    alatx = np.arccos(np.abs(np.sum(dxo * nvectx, axis=-1)) / np.linalg.norm(dxo, axis=-1))
 
     return ovectx, nvectx, t0tx, doftx, alatx
 
@@ -299,7 +300,7 @@ def make_synthetic_points_2D(steers, r0, ctx, cm, xele, xout, zout, fnum):
     pvec = np.ascontiguousarray(np.array([Px.flatten(), Pz.flatten()]).T)
 
     if r0 == 0: ovectx, nvectx, t0tx, doftx, alatx = calc_tx_synthetic_points_pw_2D(steers, ctx, cm, xele)
-    else: ovectx, nvectx, t0tx, doftx, alatx = calc_tx_synthetic_points_2D(steers, ctx, cm, xele, r0)
+    elif r0 < 0: ovectx, nvectx, t0tx, doftx, alatx = calc_tx_synthetic_points_divergent_2D(steers, ctx, cm, xele, r0)
 
     ovecrx, nvecrx, alarx = calc_rx_synthetic_points(xele, fnum)
 