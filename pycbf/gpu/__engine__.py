951390fbd6fd0585441185cd45d01ddd08e26eba
Wren Wightman <wren.wightman+github@gmail.com>
2025-12-23 20:14:14 -0500

    Added import for the coherence beamformer and numpy structures that are needed for the GPU code

--- pycbf/gpu/__engine__.py
+++ pycbf/gpu/__engine__.py
@@ -1,4 +1,5 @@
 import cupy as cp
+import numpy as np
 import os
 
 __eng_dir__ = os.path.join(os.path.dirname(__file__), "__engine__.cu")
@@ -6,4 +7,8 @@ with open(__eng_dir__, mode='r') as fp: raw_module = fp.read()
 
 module = cp.RawModule(code=raw_module)
 
+xInfo = np.dtype([('x0', np.float32),('dx', np.float32),('nx', np.int32)])
+RFInfo = np.dtype([('ntx', np.int32),('nrx', np.int32),('ndim', np.int32),('tInfo', xInfo)])
+
 das_bmode_cubic = module.get_function("das_bmode_cubic")
+das_bmode_rxseparate_cubic = module.get_function("das_bmode_rxseparate_cubic")
