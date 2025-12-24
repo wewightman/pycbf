6ff93813cbdd5e02c7a0620e58e5a841d04a8f6d
Wren Wightman <wren.wightman+github@gmail.com>
2025-12-23 20:12:09 -0500

    slay

--- pycbf/__init__.py
+++ pycbf/__init__.py
@@ -1,4 +1,1 @@
-import os as _os
-import pycbf.trig
 from pycbf.Beamformer import Beamformer, __BMFRM_PARAMS__, __BMFRM_DEBUG__
-from pycbf.PWBeamformer import PWBeamformer
--- pycbf/trig/__init__.py
+++
@@ -1,1 +0,0 @@
-from pycbf.trig.trig import *
