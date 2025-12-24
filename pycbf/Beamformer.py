f9eb4bd82ae76a653b042f77c81436662e1c2e87
Wren Wightman <wren.wightman+github@gmail.com>
2025-12-23 20:12:06 -0500

    Converted global data structure to dictionary

--- pycbf/Beamformer.py
+++ pycbf/Beamformer.py
@@ -4,14 +4,14 @@ global __BMFRM_DEBUG__
 __BMFRM_DEBUG__ = False
 
 global __BMFRM_PARAMS__
-__BMFRM_PARAMS__ = []
+__BMFRM_PARAMS__ = dict()
 
 class Beamformer(ABC):
     __bmfrm_id_counter__ = int(0)
 
     def __init__(self):
         self.id = int(Beamformer.__bmfrm_id_counter__)
-        __BMFRM_PARAMS__.append(dict())
+        __BMFRM_PARAMS__[self.id] = dict()
         Beamformer.__bmfrm_id_counter__ += int(1)
 
     @abstractmethod
