5988de0cc67003894bd6c7b9cac7990eea63137a
Wren Wightman <wren.wightman+github@gmail.com>
2025-12-23 20:12:28 -0500

    Maybe working beamformer

--- examples/cupy_module.cu
+++ examples/cupy_module.cu
@@ -141,13 +141,118 @@ extern "C" {
     struct RFInfo {
         int ntx;
         int nrx;
-        int np;
         int ndim;
         struct xInfo tInfo;
     };
 
-    __global__ void beamform(const struct RFInfo rfinfo, const float* rfdata, )
+    void calc_tautx_apodtx(
+        const int    ndim,  // 2 or 3 dimensions
+        const float*  foc,  // focal spot
+        const float* nvec,  // normal vector of wave propagation
+        const float    c0,  // assumed speed of sound in media
+        const float    t0,  // the time at which the wave reaches foc
+        const float   ala,  // the acceptance angle relative to nvec - zero for plane wave
+        const float   dof,  // the dof around foc over which to "flatten" the delay tabs
+        const float* pvec,  // the point at which we are calculating delay tabs and apodization
+        float* tau, float* apod // output numbers
+    )
     {
+        
+        // calculate the magnitude of dx and its projection onto nvec
+        float dxi, dxmag, dxproj;
+        dxmag  = 0.0;
+        dxproj = 0.0;
+        for (int idim = 0; idim < ndim; ++idim)
+        {
+            dxi = pvec[idim] - foc[idim];
+            dxmag  += dxi * dxi;
+            dxproj += dxi * nvec[idim];
+        }
+        dxmag = sqrt(dxmag);
 
+        // if synthetic focal point (diverging or converging waves)
+        if (0.0 != ala)
+        {
+            if ((0.0 != dof) && (abs(dxproj) <= dof/2)) {
+                *tau = 2.0*(dxproj/dof)*(dxmag/c0) + t0;
+                if (sqrt(dxmag*dxmag - dxproj*dxproj) <= dof * sin(ala)) *apod = 1.0;
+                else *apod = 0.0;
+            }
+            else {
+                *tau = (dxproj/abs(dxproj)) * (dxmag/c0) + t0;
+                if ((dxmag != 0.0) && (acos(abs(dxproj/dxmag)) > ala)) *apod = 1.0;
+                else *apod = 0.0;
+            }
+        }
+
+        // plane wave case
+        else {
+            *tau = dxproj/c0;
+            *apod = 1.0;
+        }
+    }
+
+
+    /**
+     * das_bmode_cubic: beamform a DAS bmode 
+     * 
+     * RF channel data parameters:
+     *   rfinfo: information about the rfdata
+     *   rfdata: grid of rf data in the shape ntx by nrx by nt (stored in tInfo)
+     * 
+     * Transmit parameters:
+     *   ovectx: origin of each transmision
+     *   nvectx: normal vector of each of the transmit events
+     *   t0tx: the timepoint at which the wave is at ovectx in each transmit event
+     *   alatx: acceptance angle in radians relative to each nvectx
+     * 
+     * Receive parameters:
+     *   ovecrx: origin of the receive points
+     *   nvecrx: normal vector of each receive element
+     *   alarx:  angular acceptance for each element relative to nvecrx
+     * 
+     * Field parameters:
+     *   c0: the homogeneos speed of sound in the medium
+     *   np: the number of recon points
+     *   pvec: the location of each recon point
+     *   pout: a vector length p for the output bmode
+     */
+    __global__ 
+    void das_bmode_cubic(
+        const struct RFInfo rfinfo, const float* rfdata, 
+        const float* ovectx, const float* nvectx, const float* t0tx, const float* alatx, const float* doftx, 
+        const float* ovecrx, const float* nvecrx, const float* alarx,
+        const float c0, const int np, const float* pvec, float* pout
+    )
+    {
+        int tpb, bpg, tid, itx, irx, ip;
+        float tautx, apodtx, taurx, apodrx;
+
+        // get cuda step sizes
+        tpb = blockDim.x * blockDim.y * blockDim.z; // threads per block
+
+        // Unique thread ID
+        tid = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y;
+        tid += tpb * blockIdx.x + tpb * blockIdx.y * gridDim.x + tpb * blockIdx.z * gridDim.x * gridDim.y;
+
+        if (tid >= rfinfo.ntx * rfinfo.nrx * np) return;
+
+        // calculate the transmit, recieve, and recon point indices for the thread we are working with
+        itx = tid / (rfinfo.nrx * np);
+        irx = (tid / np) % rfinfo.nrx;
+        ip  = tid % np;
+
+        calc_tautx_apodtx(
+            rfinfo.ndim, 
+            &ovectx[itx*rfinfo.ndim], 
+            &nvectx[itx*rfinfo.ndim], 
+            c0, t0tx[itx], 
+            alatx[itx], 
+            doftx[itx], 
+            &pvec[ip*rfinfo.ndim],
+            &tautx, &apodtx
+        );
+
+        pout[ip] = tautx * apodtx;
     }
 }
