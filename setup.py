4e0414364ee72bb518fe09f11d71eadf8670b20c
Wren Wightman <wren.wightman+github@gmail.com>
2025-12-23 20:12:02 -0500

    Created dependency structure for this package

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import Extension, setup
 
+# load requirements list
+with open("requirements.txt", 'r') as f:
+    reqs = f.readlines()
+
+with open("req_depends.txt", 'r') as f:
+    reqdeps = f.readlines()
+
+# load the C extentsion library
 trig = Extension(
     name="pycbf.trig._trig",
     include_dirs=["pycbf/trig"],
@@ -7,11 +15,14 @@ trig = Extension(
     sources=["pycbf/trig/trigengines.c"]
 )
 
+# run setup tools
 setup(
     name='pycbf',
     description="C-Backed beamforming engines",
     author_email="wew12@duke.edu",
     packages=['pycbf', 'pycbf.trig'],
+    install_requires=reqs,
+    dependency_links=reqdeps,
     ext_modules=[trig],
     version="0.0.0"
 )
